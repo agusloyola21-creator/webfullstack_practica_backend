7a733eeca51ad9ef49dfa64ba3cc3999aea6333d
Jorge Agustin Loyola <agusloyola21@gmail.com>
2025-10-14 23:12:08 -0300

    corregimos cors

--- core/settings.py
+++ core/settings.py
@@ -225,11 +225,11 @@ if not DEBUG:
     ALLOWED_HOSTS = env.list('ALLOWED_HOSTS_DEPLOY')
     # CORS_ORIGIN_WHITELIST = env.list('CORS_ORIGIN_WHITELIST_DEPLOY')
     CORS_ALLOWED_ORIGINS = [
-        "https://gregarious-peony-84c0ff.netlify.app"
+        "https://gregarious-peony-84c0ff.netlify.app/"
     ]
     # CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS_DEPLOY')    
 
     CSRF_TRUSTED_ORIGINS = [
-        "https://gregarious-peony-84c0ff.netlify.app"
+        "https://gregarious-peony-84c0ff.netlify.app/"
     ]
 
